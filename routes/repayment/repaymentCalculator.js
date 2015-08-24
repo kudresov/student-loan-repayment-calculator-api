5e940e0cb7741b2bebf3785fd1436ed1679f254f
Vitalij Kudresov <kudresov@gmail.com>
2015-08-24 23:42:13 +0100

    added total loan info

---
+++ routes/repayment/repaymentCalculator.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var _ = require('underscore');
+
+var tuitionFees = [
+  {
+    startYear: 2005,
+    fee: 1175
+  },
+  {
+    startYear: 2006,
+    fee: 3000
+  },
+  {
+    startYear: 2007,
+    fee: 3070
+  },
+  {
+    startYear: 2008,
+    fee: 3145
+  },
+  {
+    startYear: 2009,
+    fee: 3225
+  },
+  {
+    startYear: 2010,
+    fee: 3225 // TODO set correct one
+  },
+  {
+    startYear: 2011,
+    fee: 3375
+  }
+];
+
+var interestRates = [
+  {
+    startYear: 2014,
+    rate: 1.5
+  },
+  {
+    startYear: 2013,
+    rate: 1.5
+  },
+  {
+    startYear: 2012,
+    rate: 1.5
+  },
+  {
+    startYear: 2011,
+    rate: 1.5
+  },
+  {
+    startYear: 2010,
+    rate: 1.5
+  }
+// 2009/10 0.0
+// 6 March 2009 - 31 August 2009 1.5
+// 6 February 2009 - 5 March 2009  2.0
+// 9 January 2009 - 5 February 2009  2.5
+// 5 December 2008 - 8 January 2009  3.0
+// 1 September 2008 - 4 December 2008  3.8
+// 2007/08 4.8
+// 2006/07 2.4
+// 2005/06 3.2
+// 2004/05 2.6
+// 2003/04 3.1
+// 2002/03 1.3
+// 2001/02 2.3
+// 2000/01 2.6
+// 1999/00 2.1
+// 1998/99 3.5
+];
+
+module.exports.calculate = function(test){
+  console.log(test);
+};
+
+module.exports.calculateTotalLoan = function(studyYears){
+  var tuitionYearsFilter = function(studyYear){
+    if (_.contains(studyYears, studyYear.startYear)) {
+      return true;
+    }else {
+      return false;
+    }
+  };
+
+  var sum = function(memo, fee){
+    return memo + fee;
+  };
+
+  var totalLoan = _.filter(tuitionFees, tuitionYearsFilter)
+                         .map(function(tuitionYear){
+                           return tuitionYear.fee;
+                         })
+                         .reduce(sum);
+
+  return totalLoan;
+};
--- tests/repayment.spec.js
+++ tests/repayment.spec.js
@@ -30,26 +30,70 @@ describe('/repayment', function(){
       });
     });
 
-    describe('with 2008-2011 study years and 30k salary for 12 months starting on 1st of September', function(){
-      it('should return final repayment on', function(done){
-        var requestBody = {
-            studyYears: [2008, 2009, 2010],
-            jobs: [
-                {
-                    startDate: '2011-09-01T00:00:00.000Z',
-                    basicSalary: 30000
-                }
-            ]
-        };
+    describe('with study years 2008,2009,2010', function(){
+
+      it('should have a total loan of 9745£', function(done){
+          var requestBody = {
+              studyYears: [2008, 2009, 2010]
+          };
 
         repaymentRequest()
-          .send(requestBody)
+          .send(requestBody)            
           .end(function(err, res) {
             expect(res.status).to.equal(201);
+            expect(res.body.totalLoan).to.equal(9595);
             done();
           });
-
       });
+
+      describe('with 2008-2011 study years and 30k salary for 12 months starting on 1st of September 2011', function(){
+        it('should have repayed 569.93£', function(done){
+          var requestBody = {
+              studyYears: [2008, 2009, 2010],
+              jobs: [
+                  {
+                      startDate: '2011-09-01T00:00:00.000Z',
+                      endDate: '2012-09-01T00:00:00.000Z',
+                      basicSalary: 30000
+                  }
+              ]
+          };
+
+          repaymentRequest()
+            .send(requestBody)
+            .end(function(err, res) {
+              expect(res.status).to.equal(201);
+              expect(res.body.totalRepaid).to.equal(569.93);
+              done();
+            });
+
+        });
+      });
+
+      describe('with 2008-2011 study years and 30k salary for 24 months starting on 1st of September 2011', function(){
+        xit('should have repayed 1709.78£', function(done){
+          var requestBody = {
+              studyYears: [2008, 2009, 2010],
+              jobs: [
+                  {
+                      startDate: '2011-09-01T00:00:00.000Z',
+                      endDate: '2013-09-01T00:00:00.000Z',
+                      basicSalary: 30000
+                  }
+              ]
+          };
+
+          repaymentRequest()
+            .send(requestBody)
+            .end(function(err, res) {
+              expect(res.status).to.equal(201);
+              expect(res.body.totalRepaid).to.equal(1709.78);
+              done();
+            });
+
+        });
+      });
+      
     });
   });
 });
